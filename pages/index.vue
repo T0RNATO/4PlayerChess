3a21646fd1fbe33cbfb687fc26ebae8324700421
nathan.little <nlittle315@gmail.com>
2023-02-28 12:08:02 +1100

    removed button :)

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <button @click="skipTurn" class="absolute bg-white rounded-md p-4 top-2 left-2">Skip Player</button>
     <div class="w-full flex justify-center h-[100vh] items-center">
       <Game :board="this.board" :selected="this.$store.state.selected" ref="game"/>
     </div>
